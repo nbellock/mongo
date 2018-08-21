32db809cc1e6257dea3b7ee9b4a8b6fa75736c26
nbellock <nbellock@gmail.com>
2018-08-21 15:09:10 -0600

    message

--- server.js
+++ server.js
@@ -1,42 +1,51 @@
 //dependencies
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var logger = require('morgan');
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var logger = require("morgan");
 
 //initialize Express app
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 
-app.use(express.static(process.cwd() + '/public'));
+app.use(express.static(process.cwd() + "/public"));
 
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({
+    defaultLayout: "main"
 }));
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 //connecting to MongoDB
-// mongoose.connect('mongodb://heroku_rx2bjrd1:v3mta9fv4cmsq66enussc2b35q@ds227352.mlab.com:27352/heroku_rx2bjrd1');
-//try this one first vvv :)
+// mongoose.connect("mongodb://heroku_rx2bjrd1:v3mta9fv4cmsq66enussc2b35q@ds227352.mlab.com:27352/heroku_rx2bjrd1");
+
 var url = process.env.MONGOLAB_URI
-// mongoose.connect('mongodb://localhost/');
-mongoose.connect(url);
+if (!url) {
+    mongoose.connect("mongodb://localhost/");
+} else {
+    // mongoose.connect("mongodb://localhost/");
+    mongoose.connect(url);
+}
+
 
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-    console.log('Connected to Mongoose!')
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+    console.log("Connected to Mongoose!")
 });
 
-var routes = require('./controller/controller.js');
-app.use('/', routes);
+var routes = require("./controller/controller.js");
+app.use("/", routes);
 
-var port = process.env.PORT || 3001;
-app.listen(port, function () {
-    console.log('Listening on PORT ' + port);
+// var port = process.env.PORT || 3001;
+// app.listen(port, function () {
+//     console.log("Listening on PORT " + port);
+// });
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
