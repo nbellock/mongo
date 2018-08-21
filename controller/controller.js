499b2e3a24ca500315d1d9aae85e042b60c077d6
nbellock <nbellock@gmail.com>
2018-08-21 15:26:28 -0600

    'message'

--- controller/controller.js
+++ controller/controller.js
@@ -1,49 +1,49 @@
 //dependencies
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var path = require('path');
+var path = require("path");
 
 //require request and cheerio to scrape
-var request = require('request');
-var cheerio = require('cheerio');
+var request = require("request");
+var cheerio = require("cheerio");
 
 //Require models
-var Comment = require('../models/Comment.js');
-var Article = require('../models/Article.js');
+var Comment = require("../models/Comment.js");
+var Article = require("../models/Article.js");
 
 //index
-router.get('/', function (req, res) {
-    res.redirect('/articles');
+router.get("/", function (req, res) {
+    res.redirect("/articles");
     // res.render("main");
 });
 
-// router.get('/test-scrape', function(req, res) {
+// router.get("/test-scrape", function(req, res) {
 //   request(result.link, function(error, response, html) {
 //     var $ = cheerio.load(html);
 
-//     $('.l-col__main').each(function(i, element){
+//     $(".l-col__main").each(function(i, element){
 //       var result = {};
 
-//       console.log($(this).children('.c-entry-content').children('p').text());
+//       console.log($(this).children(".c-entry-content").children("p").text());
 //     });
 //   });
 // });
 
 // A GET request to scrape the Verge website
-router.get('/scrape', function (req, res) {
+router.get("/scrape", function (req, res) {
     // First, we grab the body of the html with request
-    request('http://www.theverge.com/tech', function (error, response, html) {
+    request("http://www.theverge.com/tech", function (error, response, html) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(html);
         var titlesArray = [];
         // Now, we grab every article
-        $('.c-entry-box--compact__title').each(function (i, element) {
+        $(".c-entry-box--compact__title").each(function (i, element) {
             // Save an empty result object
             var result = {};
 
             // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this).children('a').text();
-            result.link = $(this).children('a').attr('href');
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
 
             //ensures that no empty title or links are sent to mongodb
             if (result.title !== "" && result.link !== "") {
@@ -77,22 +77,22 @@ router.get('/scrape', function (req, res) {
                 }
                 // Log that scrape is working, just the content was missing parts
                 else {
-                    console.log('Article already exists.')
+                    console.log("Article already exists.")
                 }
 
             }
             // Log that scrape is working, just the content was missing parts
             else {
-                console.log('Not saved to DB, missing data')
+                console.log("Not saved to DB, missing data")
             }
         });
         // after scrape, redirects to index
-        res.redirect('/');
+        res.redirect("/");
     });
 });
 
 //this will grab every article an populate the DOM
-router.get('/articles', function (req, res) {
+router.get("/articles", function (req, res) {
     //allows newer articles to be on top
     Article.find().sort({
             _id: -1
@@ -105,13 +105,13 @@ router.get('/articles', function (req, res) {
                 var artcl = {
                     article: doc
                 };
-                res.render('index', artcl);
+                res.render("index", artcl);
             }
         });
 });
 
 // This will get the articles we scraped from the mongoDB in JSON
-router.get('/articles-json', function (req, res) {
+router.get("/articles-json", function (req, res) {
     Article.find({}, function (err, doc) {
         if (err) {
             console.log(err);
@@ -122,19 +122,19 @@ router.get('/articles-json', function (req, res) {
 });
 
 //clear all articles for testing purposes
-router.get('/clearAll', function (req, res) {
+router.get("/clearAll", function (req, res) {
     Article.remove({}, function (err, doc) {
         if (err) {
             console.log(err);
         } else {
-            console.log('removed all articles');
+            console.log("removed all articles");
         }
 
     });
-    res.redirect('/articles-json');
+    res.redirect("/articles-json");
 });
 
-router.get('/readArticle/:id', function (req, res) {
+router.get("/readArticle/:id", function (req, res) {
     var articleId = req.params.id;
     var hbsObj = {
         article: [],
@@ -145,10 +145,10 @@ router.get('/readArticle/:id', function (req, res) {
     Article.findOne({
             _id: articleId
         })
-        .populate('comment')
+        .populate("comment")
         .exec(function (err, doc) {
             if (err) {
-                console.log('Error: ' + err);
+                console.log("Error: " + err);
             } else {
                 hbsObj.article = doc;
                 var link = doc.link;
@@ -156,11 +156,11 @@ router.get('/readArticle/:id', function (req, res) {
                 request(link, function (error, response, html) {
                     var $ = cheerio.load(html);
 
-                    $('.l-col__main').each(function (i, element) {
-                        hbsObj.body = $(this).children('.c-entry-content').children('p').text();
+                    $(".l-col__main").each(function (i, element) {
+                        hbsObj.body = $(this).children(".c-entry-content").children("p").text();
                         //send article body and comments to article.handlbars through hbObj
-                        res.render('article', hbsObj);
-                        //prevents loop through so it doesn't return an empty hbsObj.body
+                        res.render("article", hbsObj);
+                        //prevents loop through so it doesn"t return an empty hbsObj.body
                         return false;
                     });
                 });
@@ -170,7 +170,7 @@ router.get('/readArticle/:id', function (req, res) {
 })
 
 // Create a new comment
-router.post('/comment/:id', function (req, res) {
+router.post("/comment/:id", function (req, res) {
     var user = req.body.name;
     var content = req.body.comment;
     var articleId = req.params.id;
@@ -194,7 +194,7 @@ router.post('/comment/:id', function (req, res) {
                     "_id": req.params.id
                 }, {
                     $push: {
-                        'comment': doc._id
+                        "comment": doc._id
                     }
                 }, {
                     new: true
@@ -204,7 +204,7 @@ router.post('/comment/:id', function (req, res) {
                     if (err) {
                         console.log(err);
                     } else {
-                        res.redirect('/readArticle/' + articleId);
+                        res.redirect("/readArticle/" + articleId);
                     }
                 });
         }
